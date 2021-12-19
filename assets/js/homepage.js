f12cf181cd96573abd624f4a7421cb5e5b1630c2
ryanweiler92 <ryanweiler92@gmail.com>
2021-12-18 21:39:45 -0500

    add form handler function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -13,8 +13,31 @@ console.log("outside");
 };  
 
 
-console.log(getUserRepos("microsoft"));
-console.log(getUserRepos("ryanweiler92"));
 
 
-getUserRepos();
+//variable to get the user form
+var userFormEl = document.querySelector("#user-form");
+//variable to get the input of the userform
+var nameInputEl = document.querySelector("#username");
+
+
+
+
+//function to recieve the information from the user form.. 
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    
+//variable stores the input of user form
+    var username = nameInputEl.value.trim();
+
+//if a username is entered, run the getUserRepos function with username value; reset the input element
+    if (username) {
+        getUserRepos(username)
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+//when the submit button is pressed the formSubmitHandler function runs
+userFormEl.addEventListener("submit", formSubmitHandler);
